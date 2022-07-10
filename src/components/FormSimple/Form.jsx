3dc9a40628b49e87fe0dc03f6fb037cd4abc5c5a
ChristianMLand <christianmland@outlook.com>
2022-07-10 11:20:37 -0700

    made simple form more dynamic

--- src/components/FormSimple/Form.jsx
+++ src/components/FormSimple/Form.jsx
@@ -3,44 +3,64 @@ import './Form.css';
 
 const Form = () => {
     const [users, setUsers] = useState([]);
+    // this simple approach only allows for a single validation type per input, see the more complex form for a way to allow for multiple
     const [formData, setFormData] = useState({
         "firstName" : {
+            "type" : "text",
+            "label" : "First Name",
             "value" : "",
-            "validation" :  { "type" : "minLength", "comparator" : 2 }
+            "validation" :  { "type" : "minLength", "comparator" : 2 },
+            "error" : ""
         },
         "lastName" : {
+            "type" : "text",
+            "label" : "Last Name",
             "value" : "",
-            "validation" : { "type" : "minLength", "comparator" : 2 }
+            "validation" : { "type" : "minLength", "comparator" : 2 },
+            "error" : ""
         },
         "email" : {
+            "type" : "email",
+            "label" : "Email",
             "value" : "",
-            "validation" : { "type" : "minLength", "comparator" : 5 }
+            "validation" : { "type" : "minLength", "comparator" : 5 },
+            "error" : ""
         },
         "password" : {
+            "type" : "password",
+            "label" : "Password",
             "value" : "",
-            "validation" : { "type" : "minLength", "comparator" : 8 }
+            "validation" : { "type" : "minLength", "comparator" : 8 },
+            "error" : ""
         },
         "confirmPassword" : {
+            "type" : "password",
+            "label" : "Confirm Password",
             "value" : "",
-            "validation" : { "type" : "matches", "comparator" : "password" }
+            "validation" : { "type" : "matches", "comparator" : "password" },
+            "error" : ""
         }
     });
 
     const handleChange = e => {
-        const copy = structuredClone(formData)
-        copy[e.target.name].value = e.target.value;
+        const copy = structuredClone(formData) // does a deep copy of the current state before we modify it
+        const { name, value } = e.target;
+        copy[name].value = value;
+        copy[name].error = validate(name, value)
         setFormData(copy)
     };
 
-    const validate = (inputName) => {
-        const { type, comparator } = formData[inputName].validation;
+    const validate = (name, value) => {
+        const { label, validation } = formData[name]
+        const { type, comparator } = validation;
+        // simply add more cases to the switch statement here if more validation types are needed
         switch (type) {
             case "minLength":
-                return formData[inputName].value.length < comparator ?  <p>{`${inputName} should be at least ${comparator} characters long`}</p> : ""
+                return value.length < comparator ? `${label} should be at least ${comparator} characters long` : ""
             case "maxLength":
-                return formData[inputName].value.length > comparator ?  <p>{`${inputName} should be no more than ${comparator} characters long`}</p> : ""
+                return value.length > comparator ?  `${label} should be no more than ${comparator} characters long` : ""
             case "matches":
-                return formData[inputName].value !== formData[comparator].value ? <p>{`${inputName} should match ${comparator}`}</p> : ""
+                return value !== formData[comparator].value ?`${label} should match ${comparator}` : ""
             default:
                 return ""
         }
@@ -48,8 +68,15 @@ const Form = () => {
 
     const createUser = e => {
         e.preventDefault()
-        if (Object.keys(formData).every(input => !validate(input))) {
-            setUsers([...users, Object.entries(formData).reduce((prev, [name, { value }]) => { prev[name] = value; return prev }, {})])
+        // this condition will be false if any input has an error
+        if (Object.keys(formData).every(input => !formData[input].error)) {
+            // this creates an object mapping the input names to their values
+            const userData = Object.entries(formData).reduce((prev, inputData) => {
+                const [name, { value }] = inputData;
+                prev[name] = value; 
+                return prev 
+            }, {})
+            setUsers([...users, userData])
         } else {
             alert("Invalid Form")
         }
@@ -59,41 +86,15 @@ const Form = () => {
         <form className="form" onSubmit={ createUser }>
             <fieldset>
                 <legend>Register</legend>
-                <div className="form-input">
-                    <div>
-                        <label>First Name</label>
-                        <input type="text" name="firstName" value={ formData.firstName.value } onChange={ handleChange }/>
+                { Object.entries(formData).map(([name, {label, type, value, error}], i) => 
+                    <div key={ i } className="form-input">
+                        <div>
+                            <label>{ label }</label>
+                            <input type={ type } name={ name } value={ value } onChange={ handleChange }/>
+                        </div>
+                        { value && <p>{ error }</p> }
                     </div>
-                    { formData.firstName.value && validate("firstName") }
-                </div>
-                <div className="form-input">
-                    <div>
-                        <label>Last Name</label>
-                        <input type="text" name="lastName" value={ formData.lastName.value } onChange={ handleChange }/>
-                    </div>
-                    { formData.lastName.value && validate("lastName") }
-                </div>
-                <div className="form-input">
-                    <div>
-                        <label>Email</label>
-                        <input type="email" name="email" value={ formData.email.value } onChange={ handleChange }/>
-                    </div>
-                    { formData.email.value && validate("email") }
-                </div>
-                <div className="form-input">
-                    <div>
-                        <label>Password</label>
-                        <input type="password" name="password" value={ formData.password.value } onChange={ handleChange }/>
-                    </div>
-                    { formData.password.value && validate("password") }
-                </div>
-                <div className="form-input">
-                    <div>
-                        <label>Confirm Password</label>
-                        <input type="password" name="confirmPassword" value={ formData.confirmPassword.value } onChange={ handleChange }/>
-                    </div>
-                    { formData.confirmPassword.value && validate("confirmPassword") }
-                </div>
+                )}
                 <button>Submit</button>
             </fieldset>
             { users.map((user, i) => <p key={ i }>{user.firstName} {user.lastName}</p>)}
